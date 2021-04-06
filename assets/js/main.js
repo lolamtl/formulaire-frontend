fa322686bb4e649a9701a04dd6b992bd63aa5409
Lola <lmonteil@live.fr>
2021-04-06 18:17:47 +0200

    chnage style/add background img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("page refresh");
   document
     .querySelector("#contact-form")
     .addEventListener("submit", async (event) => {
       event.preventDefault();
       const data = {
-        fisrtname: document.querySelector("#firstname").value,
+        firstname: document.querySelector("#firstname").value,
         lastname: document.querySelector("#lastname").value,
         email: document.querySelector("#email").value,
         subject: document.querySelector("#subject").value,
         message: document.querySelector("#message").value,
       };
-      console.log(data);
+      // console.log(data.message.value);
       const response = await axios.post(
-        "https://formulairebackend.herokuapp.com/",
+        // "https://formulairebackend.herokuapp.com/",
+        "http://localhost:3020/",
         data
       );
-      console.log(response);
-
-      if (response.status !== 200) {
-        alert("Nop ca marche pas!");
+      if (response.status === 200) {
+        alert(
+          "Votre message a bien été envoyé nous traiterons votre demande dans les plus brefs délais."
+        );
+      } else if (!response) {
+        alert("Réessayer en remplissant tous les champs obligatoire");
       } else {
-        alert("C'est envoyé !");
+        alert("Réessayer en remplissant tous les champs obligatoire");
       }
     });
 });
